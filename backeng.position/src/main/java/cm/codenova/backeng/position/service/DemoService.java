a7a972b4cb5b7e38af353200d7f5c7d93b76f24f
domeni.ngameni <domeni.ngameni@lao-sarl.cm>
2024-12-26 19:57:33 +0100

    init projet

---
+++ backeng.position/src/main/java/cm/codenova/backeng/position/service/DemoService.java
@@ -0,0 +1,13 @@
+package cm.codenova.backeng.position.service;
+
+import cm.codenova.backeng.position.demo.dto.DemoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DemoService {
+    public List<DemoDTO> fetchAllDemos() {
+        return null;
+    }
+}
